8ebba65e62dc9ddac682542352a0c148a871579e
Chandler Swift <chandler@chandlerswift.com>
2018-06-30 12:32:37 -0500

    Updated flask demo server

--- drive-from-web/flask-server/flask_test.py
+++ drive-from-web/flask-server/flask_test.py
@@ -1,6 +1,28 @@
 from flask import Flask
+from flask import request
+from subprocess import call
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return """
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ACM Robot Web Control Panel</title>
+  </head>
+  <body>
+    <h1>ACM Robot Web Control Panel</h1>
+    <form method=POST action="/say">
+      Say something:
+      <input name="text" type="text">
+      <input type="submit">
+    </form>
+  </body>
+</html>"""
+
+@app.route("/say", methods=['POST'])
+def say():
+    call(['ssh', 'mobility@10.0.0.2', 'echo %s >> /dev/ttyS3' % request.form["text"]])
+    return "Okay!"
