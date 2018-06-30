d9f0198eb9c6e19f54d3e056a776a912a0a30c64
Chandler Swift <chandler@chandlerswift.com>
2018-06-30 13:52:49 -0500

    Remove unused server code

--- drive-from-web/demo-server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = require('url');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var params = url.parse(req.url, true).query;
-  console.log()
-}).listen(8080);
-
