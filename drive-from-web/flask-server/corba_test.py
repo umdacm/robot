4f6f730cd85c7a3354650053166abfbbd94fbe90
Chandler Swift <chandler@chandlerswift.com>
2018-07-04 18:23:29 -0500

    Cleanup

--- drive-from-web/flask-server/corba_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import CORBA
-orb = CORBA.ORB_init()
-o = orb.string_to_object("corbaloc::host.example.com/fortune")
-o = o._narrow(Fortune.CookieServer)
-print(o.get_cookie())
